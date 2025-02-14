337d7c829f952e4c94f614307d4464dad6856525
MEGA <MEGA@gmail.com>
2025-02-14 18:10:34 +0200

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    
+    static bool IsPowerOfTwo(int n)
+    {
+        
+        if (n == 1)
+            return true;
+
+        
+        if (n <= 0 || n % 2 != 0)
+            return false;
+
+        
+        return IsPowerOfTwo(n / 2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Въведете число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (IsPowerOfTwo(num))
+            Console.WriteLine($"{num} е степен на 2.");
+        else
+            Console.WriteLine($"{num} не е степен на 2.");
+    }
+}
